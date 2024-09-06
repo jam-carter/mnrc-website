0ec52f10d90e54a2edc507be39d02f179e123986
Jamaul Alexander Aaron <127161564+jam-carter@users.noreply.github.com>
2024-09-06 13:05:29 +0800

    Update eventCard.tsx

--- app/components/eventCard.tsx
+++ app/components/eventCard.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './eventCard.module.css';
 
-const EventCard = ( { title , description , imageSrc, altText, date}:{title: string, description: string, imageSrc: string, altText: string, date: string }) => {  return (
+const EventCard = ( { title , description , imageSrc, altText }:{title: string, description: string, imageSrc: string, altText: string }) => {  return (
     <div className={styles.card}>
 
       <div className={styles.eventCardText}>
         <h2>{title}</h2>
         <p>{description}</p>
-        <p>{date}</p>
-        
       </div>
 
       <div className={styles.eventCardImage}>
